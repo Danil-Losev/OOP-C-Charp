9bf5717cf1e6af47fdb1134d3b4654b1840cb86b
Danil Losev <dlosevdan@gmail.com>
2025-02-12 21:41:33 +0600

    commit 12/02/2025

--- Lab1/Task1/Task1/Program.cs
+++ Lab1/Task1/Task1/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Variant 6
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Write the array of numbers: ");
+        string stringOfNumbers = Console.ReadLine();
+
+        stringOfNumbers = stringOfNumbers.TrimEnd('\n', '\r');
+
+        int[] arrayOfNumbers = stringOfNumbers.Split(' ').Select(int.Parse).ToArray();
+        int evenNumbers = 0;
+        foreach (int number in arrayOfNumbers)
+        {
+            if (number % 2 == 0)
+            {
+                evenNumbers++;
+            }
+        }
+        Console.Write("Your array: ");
+        foreach (int number in arrayOfNumbers)
+        {
+            Console.Write(number + " ");
+        }
+        Console.WriteLine("\nCount of even numbers is: " + evenNumbers);
+    }
+}
--- Lab1/Task2/Program.cs
+++ Lab1/Task2/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int countOfElementes = 0;
+
+        Console.Write("Enter the count of elements: ");
+        countOfElementes = int.Parse(Console.ReadLine());
+
+        int[] arrayOfNumbers = new int[countOfElementes];
+
+        for (int i = arrayOfNumbers.GetLowerBound(0); i <= arrayOfNumbers.GetUpperBound(0); i++)
+        {
+            Console.Write("    Enter the element " + (i + 1) + ": ");
+            arrayOfNumbers[i] = int.Parse(Console.ReadLine());
+        }
+
+        int[] descendingOrderSortedArray = arrayOfNumbers.OrderDescending().ToArray();
+
+        Array taskArray = Array.CreateInstance(typeof(int), 10);
+
+        for (int i = taskArray.GetLowerBound(0); i <= taskArray.GetUpperBound(0); i++)
+        {
+            taskArray.SetValue(descendingOrderSortedArray[i] + arrayOfNumbers[i], i);
+        }
+
+        Console.Write("Array after adding elements: ");
+        foreach (int number in taskArray)
+        {
+            Console.Write(number + " ");
+        }
+    }
+}
