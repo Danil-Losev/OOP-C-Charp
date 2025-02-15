e56edf0dd4cb3ee7af98974039a9cedaccbd137d
Danil Losev <dlosevdan@gmail.com>
2025-02-15 17:53:58 +0600

    task 3 and task 4 of lab1

--- Lab1/Task3/Program.cs
+++ Lab1/Task3/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] myArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int[] result = removeElementsFromArray(myArray, [ 1, 3, 5, 7, 9 ]);
+        Console.WriteLine("Your output array is " + String.Join(" ", result));
+        Console.ReadLine();
+    }
+
+    private static int[] removeElementsFromArray(int[] inputArray, int[] elements)
+    {
+        List<int> outputArray = new List<int>();
+
+        foreach (int el in inputArray)
+        {
+            if (!elements.Contains(el))
+            {
+                outputArray.Add(el);
+            }
+        }
+
+        return outputArray.ToArray();
+    }
+}
---
+++ Lab1/Task4/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] myArray = { 1, 2, 3, -1, 5, 6, 11, 8, 9, 10 };
+        
+        Console.WriteLine("Out method:");
+        int outMax, outMin, outMid;
+        outMaxMinMid(myArray, out outMax, out outMin, out outMid);
+        Console.WriteLine("Max out: " + outMax + '\n' + "Min out: " + outMin + '\n' + "Mid out: " + outMid);
+
+        Console.WriteLine("\nVar method:");
+        var MaxMinMidTuple = varMaxMinMid(myArray);
+        Console.WriteLine("Max var: " + MaxMinMidTuple.Item1 + '\n' + "Min var: " + MaxMinMidTuple.Item2 + '\n' + "Mid var: " + MaxMinMidTuple.Item3);
+    }
+    private static void outMaxMinMid(int[] inputArray, out int maxElement, out int minElement, out int midElement)
+    {
+        int[] tempArray = inputArray.Where(x => x>0).ToArray();
+        maxElement = tempArray.Max();
+        minElement = tempArray.Min();
+        midElement = Convert.ToInt32(tempArray.Average());
+    }
+    private static (int, int, int) varMaxMinMid(int[] inputArray)
+    {
+        int[] tempArray = inputArray.Where(x => x > 0).ToArray();
+        var outputVar = (tempArray.Max(), tempArray.Min(), Convert.ToInt32(tempArray.Average()));
+        return outputVar;
+    }
+}
