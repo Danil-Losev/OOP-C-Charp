abbaa49dc933420d513381304b26d8b2ab2bb235
Danil Losev <dlosevdan@gmail.com>
2025-03-08 17:14:42 +0600

    added lab 5 commit 08.03.2025

---
+++ Lab5/Task1/Task1/Edition.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Edition
+    {
+        protected string name;
+        protected System.DateTime releaseDate;
+        protected int circulation;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public System.DateTime ReleaseDate
+        {
+            get { return releaseDate; }
+            set { releaseDate = value; }
+        }
+        public int Circulation
+        {
+            get { return circulation; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("Circulation must be a non-negative number.");
+                }
+                circulation = value;
+            }
+        }
+
+        public Edition()
+        {
+            Name = "N/A";
+            ReleaseDate = DateTime.Now;
+            Circulation = 0;
+        }
+
+        public Edition(string nameOfEdition, System.DateTime releaseDate, int circulation)
+        {
+            Name = nameOfEdition;
+            ReleaseDate = releaseDate;
+            Circulation = circulation;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType()) return false;
+            Edition edition = (Edition)obj;
+            return edition.Name == Name && edition.ReleaseDate == ReleaseDate && edition.Circulation == Circulation;
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() + ReleaseDate.GetHashCode() + Circulation.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return "Name of edition: " + Name + ", Release date: " + ReleaseDate + ", Circulation: " + Circulation;
+        }
+
+        public static bool operator ==(Edition edition1, Edition edition2)
+        {
+            if (ReferenceEquals(edition1, edition2)) return true;
+            if ((object)edition1 == null || (object)edition2 == null) return false;
+            return edition1.Name == edition2.Name && edition1.ReleaseDate == edition2.ReleaseDate && edition1.Circulation == edition2.Circulation;
+
+        }
+        public static bool operator !=(Edition edition1, Edition edition2)
+        {
+            if ((object)edition1 == null || (object)edition2 == null) return true;
+            return edition1.Name != edition2.Name || edition1.ReleaseDate != edition2.ReleaseDate || edition1.Circulation != edition2.Circulation;
+        }
+    }
+}
---
+++ Lab5/Task1/Task1/Program.cs
@@ -0,0 +1,78 @@
+﻿
+
+using System.Collections;
+
+namespace Task1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Edition firstEdition = new Edition("Edition", new DateTime(2025, 01, 01), 1000);
+            Edition secondEdition = new Edition("Edition", new DateTime(2025, 01, 01), 1000);
+
+            Console.WriteLine("Comparing the reference of to objects: " + ReferenceEquals(firstEdition, secondEdition));
+            Console.WriteLine("Comparing the values of two objects: " + firstEdition.Equals(secondEdition));
+            Console.WriteLine("Hash code of the first object: " + firstEdition.GetHashCode());
+            Console.WriteLine("Hash code of the second object: " + secondEdition.GetHashCode());
+
+            try { firstEdition.Circulation = -1; }
+            catch (Exception exception) { Console.WriteLine(exception.Message); }
+
+            Magazine firstMagazine = new Magazine("Times", Frequency.Monthly, new DateTime(2025, 01, 01), 1000);
+            firstMagazine.AddArticles(new Article(new Person("Danil", "Losev", new DateTime(2025, 01, 01)), "First article", 100),
+                new Article(new Person("Danil", "Losev", new DateTime(2025, 01, 01)), "Second article", 200));
+            firstMagazine.AddEditors(new Person("Danil", "Losev", new DateTime(2025, 01, 01)),
+                new Person("Arthyom", "Losev", new DateTime(2025, 01, 01)),
+                new Person("Steve", "Jobes", new DateTime(2025, 01, 01)));
+
+            Console.WriteLine("\n\nMagazine info:\n" + firstMagazine);
+
+            Console.WriteLine("\nEdition property of Magazine: " + firstMagazine.Edition);
+
+            Magazine secondMagazine = (Magazine)firstMagazine.DeepCopy();
+
+            firstMagazine.Name = "New Times";
+            firstMagazine.Frequency = Frequency.Weekly;
+            Console.WriteLine("\n\nPrinting changed sourse:\n");
+            Console.WriteLine(firstMagazine);
+
+            Console.WriteLine("\nPrinting copy of sourse before changing:\n");
+            Console.WriteLine(secondMagazine);
+
+            firstMagazine.AddArticles(new Article(new Person("Danil", "Losev", new DateTime(2025, 01, 01)), "Third article", 300),
+                new Article(new Person("Arthyom", "Losev", new DateTime(2025, 01, 01)), "Third article", 400),
+                new Article(new Person("Bill", "Gates", new DateTime(2025, 01, 01)), "Fifth article", 500));
+
+            Console.WriteLine("\n\nArticles with rating more than 100");
+            foreach (Article article in firstMagazine.GetArticlesWithMoreRating(100))
+            {
+                Console.WriteLine("    " + article);
+            }
+
+            Console.WriteLine("\nArticles with title: \"Third article\"");
+            foreach (Article article in firstMagazine.GetArticlesWithSameTitle("Third article"))
+            {
+                Console.WriteLine("    " + article);
+            }
+
+            Console.WriteLine("\nArticles with authors who are not a editors");
+            foreach (Article article in firstMagazine)
+            {
+                Console.WriteLine(article);
+            }
+
+            Console.WriteLine("\nArticles with authors who are editors");
+            foreach (Article article in firstMagazine.GetArticlesOfEditors())
+            {
+                Console.WriteLine(article);
+            }
+
+            Console.WriteLine("\nEdotors without articles");
+            foreach (Person editor in firstMagazine.GetEditorsWithoutArticles())
+            {
+                Console.WriteLine(editor);
+            }
+        }
+    }
+}
