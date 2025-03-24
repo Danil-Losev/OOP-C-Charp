a34b318924f389e399292dc5b0c8111b53f4f76a
Danil Losev <dlosevdan@gmail.com>
2025-03-25 00:43:16 +0600

    commit 25.03.2025

---
+++ Lab6/Lab6/MagazineCollection.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    internal class MagazineCollection
+    {
+        private List<Magazine> magazines;
+        public void AddDefaults()
+        {
+            magazines = new List<Magazine>(){new Magazine("Forbes", Frequency.Monthly, new DateTime(2020, 1, 1), 1000),
+              new Magazine("Time", Frequency.Weekly, new DateTime(2020, 1, 1), 500) };
+        }
+        public void AddMagazines(params Magazine [] newMagazines)
+        {
+            if (magazines == null)
+            {
+                magazines = new List<Magazine>();
+            }
+            magazines.AddRange(newMagazines);
+        }
+        public override string ToString()
+        {
+            string result = "";
+            foreach (Magazine magazine in magazines)
+            {
+                result += magazine.ToString() + "\n";
+            }
+            return result;
+        }
+        public virtual string ToShortString()
+        {
+            string result = "";
+            foreach (Magazine magazine in magazines)
+            {
+                result += magazine.ToShortString() + "\n";
+            }
+            return result;
+        }
+
+        private static int nameCompare(Magazine m1, Magazine m2)
+        {
+            return m1.Edition.CompareTo(m2.Edition);
+        }
+
+        public void SortByName()
+        {
+            magazines.Sort(nameCompare);
+        }
+
+        public void SortByReleaseDate()
+        {
+            magazines.Sort(new Edition());
+        }
+
+
+        public void SortByCirculation()
+        {
+            magazines.Sort(new EditionComparer());
+        }
+    }
+}
